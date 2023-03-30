8129faa93248f975164a4991655646de122475e2
ySanerip <yuri.calzzani@gmail.com>
2023-03-30 10:02:02 -0300

    modificação select

--- backend/extrair_dados_test.php
+++ backend/extrair_dados_test.php
@@ -1,5 +1,6 @@
 <?php
 
+
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
@@ -8,7 +9,7 @@ ini_set('display_errors', '1');
 $host = 'localhost';
 $database = '/home/platonicidiot/Documents/Projects/psel-shinier-2023/BaseTeste.fdb';
 $username = 'SYSDBA';
-$password = 'masterkey';
+$password = 'p1r3n@521';
 
 try {
     $conexao = new PDO("firebird:dbname={$database};host={$host}", $username, $password);
@@ -17,18 +18,30 @@ try {
 }
 
 try {
-    $query = $conexao->prepare("SELECT NOME FROM EMD101");
+    $query = $conexao->prepare("SELECT
+ EMD101.NOME,
+ TPAGAR.DOCUMENTO,
+ CONTAS_PAGAR_BAIXA.ID_FORMA_PAGAMENTO,
+ TPAGAR.VALOR_PARCELA,
+ CONTAS_PAGAR_BAIXA.VALOR_PAGO,
+ TPAGAR.EMISSAO,
+ TPAGAR.VENCTO,
+ TPAGAR.DATA_PAGTO,
+ CONTAS_PAGAR_BAIXA.DATA_BAIXA
+ FROM TPAGAR
+ LEFT JOIN EMD101 ON TPAGAR.CNPJ_CPF = EMD101.CGC_CPF
+ LEFT JOIN CONTAS_PAGAR_BAIXA ON TPAGAR.DOCUMENTO = CONTAS_PAGAR_BAIXA.DOCUMENTO;
+");
     $query->execute();
     $dados = $query->fetchAll(PDO::FETCH_OBJ);
 
     foreach($dados as $linha){
-        echo $linha->NOME . "<br/>";
+            echo $linha->NOME.", ";
+            echo $linha->DOCUMENTO.", ";
+            echo $linha->ID_FORMA_PAGAMENTO."\n";
     }
 } catch (PDOException $e) {
     echo "Erro ao executar consulta: " . $e->getMessage();
     exit(); // exit script on error
 }
-
-
-?>
 
