04308f958ecfd0f44310f16cee41ecd581e63033
ySanerip <yuri.calzzani@gmail.com>
2023-03-30 17:06:02 -0300

    extrair token e enviar arquivo

--- backend/api_send_file.php
+++ backend/api_send_file.php
@@ -1,11 +1,13 @@
 <?php
 
+
+
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
 
 //  autenticação
-$email = 'yuri.calzzani@usp.pr';
+$email = 'yuri.calzzani@usp.br';
 $password = 'S@n3r1p@2023';
 
 
@@ -17,36 +19,53 @@ $login_data = [
     'password' => $password
 ];
 
-
-// arquivo
-$file_path = '/home/platonicidiot/Downloads/financeiro.csv';
-$file_name = 'financeiro.csv';
-
-
-// Define os dados para envio
-    $url = "https://psel.apoena.shinier.com.br/api/import/create";
-    $data = array(
-        'file' => new CURLFile($file_path),
-        'type' => 'psel-shinier-2023',
-        'erp' => 'Psel',
-        'user_id' => '3f83fe1a-8adc-40cd-9468-c2cfda716392'
-    );
-
-    // Inicializa a biblioteca cURL
+// Inicializa a biblioteca cURL
     $ch = curl_init();
 
     // Define as opções de envio
-    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_URL, $login_url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $login_data);
+
+    $result = curl_exec($ch);
+
+    $response = json_decode($result);
+    $token = $response->token;  	  
+    $user_id = $response->user->id;
+
+    var_dump($token);
+
+    curl_close($ch);
+    
+// Define os dados para envio
+    $sendfile_url = "https://psel.apoena.shinier.com.br/api/import/create";
+
+    $authorization = "Authorization: Bearer ".$token;
+
+    $data =[ 
+        'file' => new CURLFile('/home/platonicidiot/Downloads/financeiro.csv'),
+        'type' => 'psel-shinier-2023',
+        'erp' => 'Psel',
+        'user_id' => $user_id
+    ];
+     $ch = curl_init();
+
+
+    // Define as opções de envio
+    curl_setopt($ch, CURLOPT_URL, $sendfile_url);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json' , $authorization ));   
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);  
     curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 
-    // Executa a requisição
-    $result = curl_exec($ch);
+    //RECEBENDO 
+    $send_result = curl_exec($ch);
+    $send_response = json_decode($send_result);
+    var_dump($send_response);
 
     // Fecha a conexão
     curl_close($ch);
 
 
 ?>
-
