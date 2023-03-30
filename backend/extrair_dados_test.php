2275806c9c7d49cea23f2e68afc682f6eb73ca60
ySanerip <yuri.calzzani@gmail.com>
2023-03-30 10:02:38 -0300

    adição de hearders

--- backend/extrair_dados_test.php
+++ backend/extrair_dados_test.php
@@ -1,5 +1,10 @@
 <?php
 
+header("Content-type: text/csv");
+header("Content-Disposition: attachment; filename=REGION V-Survey.csv");
+header("Pragma: no-cache");
+header("Expires: 0");
+
 
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
